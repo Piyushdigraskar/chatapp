e6c64c0eb28048a1e2989ad6c1c40f1afab48c83
Piyushdigraskar <piyushdigraskar3@gmail.com>
2023-07-27 15:11:44 +0530

    setting up cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyparser = require('body-parser');
 const errorController = require('./controllers/error');
 const dotenv = require('dotenv');
@@ -12,6 +13,11 @@ const app = express();
 const Users = require('./models/user');
 
 const userRoutes = require('./routes/user');
+
+app.use(cors({
+    origin: "*",
+    credentials: true
+}));
 
 app.use(bodyparser.json());
 
